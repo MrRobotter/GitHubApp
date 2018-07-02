21de185d82add0b5c72671dc04453d489c1c13b7
zhangjunyang <2816886869@qq.com>
2018-07-02 15:12:54 +0800

    fragment_mine.xml

--- app/src/main/java/com/joinyon/githubapp/ui/module/mine/MineFragment.java
+++ app/src/main/java/com/joinyon/githubapp/ui/module/mine/MineFragment.java
@@ -4,11 +4,18 @@ package com.joinyon.githubapp.ui.module.mine;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.joinyon.githubapp.R;
+import com.joinyon.githubapp.common.ImageLoader;
+import com.joinyon.githubapp.data.model.User;
+import com.joinyon.githubapp.data.pref.AccountPref;
+import com.joinyon.githubapp.data.pref.AppPref;
 import com.joinyon.githubapp.ui.base.BaseFragment;
 
 import butterknife.BindView;
@@ -18,6 +25,12 @@ import butterknife.ButterKnife;
  * A simple {@link Fragment} subclass.
  */
 public class MineFragment extends BaseFragment {
+    @BindView(R.id.user_icon)
+    ImageView mUserIcon;
+
+    @BindView(R.id.username)
+    TextView mUsername;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,5 +38,23 @@ public class MineFragment extends BaseFragment {
         ButterKnife.bind(this, contentView);
         getActivity().setTitle("个人信息");
         return contentView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateUser();
+    }
+
+    private void updateUser() {
+        if (AccountPref.isLogin(getContext())) {
+            User user = AccountPref.getLoginUser(getContext());
+            ImageLoader.loadWithCircle(getContext(), user.getAvatar_url(), mUserIcon);
+            String displayName = TextUtils.isEmpty(user.getName()) ? user.getLogin() : user.getName();
+            mUsername.setText(displayName);
+        } else {
+            mUsername.setText("未登录");
+        }
+
     }
 }
